ba682321b9b15a43b8ddf70fbd74da84521dc61a
Maicon Moreno <maiconmoreno@hotmail.com>
2020-05-29 14:26:24 -0300

    Realizando primeiro acesso ao banco

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Iniciando app
 const app = express();
 
+
+// Iniciando Conexão com banco de dados Mongo
+
+
+mongoose
+    .connect('mongodb://localhost:27017/nodeapi', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log('Mongo Conectado!'))
+
+    .catch(erro => {
+        console.log(erro);
+    });
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// Rota 
 app.get("/", (req, res) => {
-    res.send("Hello wolrd");
+
+    Product.create({
+        title: 'React Native',
+        description: "Build native app with React",
+        url: "http://github.com/facebook/react-native"
+    })
+
+
+    return res.send("Hello wolrd");
 });
 
 
